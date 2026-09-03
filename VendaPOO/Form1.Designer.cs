dccace53e31d5098bcbb5d271d4e76f2125b9874
Lucas Alberto <100029506+alberto-Lucas@users.noreply.github.com>
2026-09-02 21:02:33 -0300

    VendaPOO

--- VendaPOO/Form1.Designer.cs
+++ VendaPOO/Form1.Designer.cs
@@ -28,13 +28,332 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAdicionar = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtVendedor = new System.Windows.Forms.TextBox();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.group = new System.Windows.Forms.GroupBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrecoUnitario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDesconto = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lstRegistros = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtFormaPagamento = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtValorPago = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblTotalVenda = new System.Windows.Forms.Label();
+            this.txtTroco = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnAdicionar.SuspendLayout();
+            this.group.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdicionar.Controls.Add(this.lblTotalVenda);
+            this.btnAdicionar.Controls.Add(this.label7);
+            this.btnAdicionar.Controls.Add(this.txtCliente);
+            this.btnAdicionar.Controls.Add(this.label2);
+            this.btnAdicionar.Controls.Add(this.txtVendedor);
+            this.btnAdicionar.Controls.Add(this.label1);
+            this.btnAdicionar.Location = new System.Drawing.Point(12, 12);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(683, 122);
+            this.btnAdicionar.TabIndex = 0;
+            this.btnAdicionar.TabStop = false;
+            this.btnAdicionar.Text = "Dados:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vendedor:";
+            // 
+            // txtVendedor
+            // 
+            this.txtVendedor.Location = new System.Drawing.Point(6, 38);
+            this.txtVendedor.Name = "txtVendedor";
+            this.txtVendedor.Size = new System.Drawing.Size(397, 20);
+            this.txtVendedor.TabIndex = 1;
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(6, 83);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(397, 20);
+            this.txtCliente.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cliente:";
+            // 
+            // group
+            // 
+            this.group.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.group.Controls.Add(this.btnRemover);
+            this.group.Controls.Add(this.button1);
+            this.group.Controls.Add(this.lstRegistros);
+            this.group.Controls.Add(this.txtDesconto);
+            this.group.Controls.Add(this.label6);
+            this.group.Controls.Add(this.txtQuantidade);
+            this.group.Controls.Add(this.label5);
+            this.group.Controls.Add(this.txtPrecoUnitario);
+            this.group.Controls.Add(this.label3);
+            this.group.Controls.Add(this.txtDescricao);
+            this.group.Controls.Add(this.label4);
+            this.group.Location = new System.Drawing.Point(12, 140);
+            this.group.Name = "group";
+            this.group.Size = new System.Drawing.Size(683, 161);
+            this.group.TabIndex = 1;
+            this.group.TabStop = false;
+            this.group.Text = "Itens:";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(6, 38);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(286, 20);
+            this.txtDescricao.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Descrição:";
+            // 
+            // txtPrecoUnitario
+            // 
+            this.txtPrecoUnitario.Location = new System.Drawing.Point(298, 38);
+            this.txtPrecoUnitario.Name = "txtPrecoUnitario";
+            this.txtPrecoUnitario.Size = new System.Drawing.Size(105, 20);
+            this.txtPrecoUnitario.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(298, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Preço Unitário:";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(409, 38);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(67, 20);
+            this.txtQuantidade.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(409, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Quantidade";
+            // 
+            // txtDesconto
+            // 
+            this.txtDesconto.Location = new System.Drawing.Point(482, 38);
+            this.txtDesconto.Name = "txtDesconto";
+            this.txtDesconto.Size = new System.Drawing.Size(105, 20);
+            this.txtDesconto.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(482, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Desconto:";
+            // 
+            // lstRegistros
+            // 
+            this.lstRegistros.FormattingEnabled = true;
+            this.lstRegistros.Location = new System.Drawing.Point(6, 64);
+            this.lstRegistros.Name = "lstRegistros";
+            this.lstRegistros.Size = new System.Drawing.Size(581, 82);
+            this.lstRegistros.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Adicionar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(593, 123);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.TabIndex = 10;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.txtTroco);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.txtValorPago);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.txtFormaPagamento);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Location = new System.Drawing.Point(12, 307);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(683, 73);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Pagamento:";
+            // 
+            // txtFormaPagamento
+            // 
+            this.txtFormaPagamento.Location = new System.Drawing.Point(6, 38);
+            this.txtFormaPagamento.Name = "txtFormaPagamento";
+            this.txtFormaPagamento.Size = new System.Drawing.Size(286, 20);
+            this.txtFormaPagamento.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 22);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(111, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Forma de Pagamento:";
+            // 
+            // txtValorPago
+            // 
+            this.txtValorPago.Location = new System.Drawing.Point(298, 38);
+            this.txtValorPago.Name = "txtValorPago";
+            this.txtValorPago.Size = new System.Drawing.Size(105, 20);
+            this.txtValorPago.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(298, 22);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 13);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "Valor Pago:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(534, 34);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Total Venda:";
+            // 
+            // lblTotalVenda
+            // 
+            this.lblTotalVenda.AutoSize = true;
+            this.lblTotalVenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalVenda.Location = new System.Drawing.Point(533, 56);
+            this.lblTotalVenda.Name = "lblTotalVenda";
+            this.lblTotalVenda.Size = new System.Drawing.Size(73, 24);
+            this.lblTotalVenda.TabIndex = 5;
+            this.lblTotalVenda.Text = "R$ 0,00";
+            // 
+            // txtTroco
+            // 
+            this.txtTroco.Location = new System.Drawing.Point(409, 38);
+            this.txtTroco.Name = "txtTroco";
+            this.txtTroco.ReadOnly = true;
+            this.txtTroco.Size = new System.Drawing.Size(105, 20);
+            this.txtTroco.TabIndex = 7;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(409, 22);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Troco:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(707, 388);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.group);
+            this.Controls.Add(this.btnAdicionar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.btnAdicionar.ResumeLayout(false);
+            this.btnAdicionar.PerformLayout();
+            this.group.ResumeLayout(false);
+            this.group.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox btnAdicionar;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtVendedor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox group;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstRegistros;
+        private System.Windows.Forms.TextBox txtDesconto;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPrecoUnitario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtFormaPagamento;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblTotalVenda;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtValorPago;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtTroco;
+        private System.Windows.Forms.Label label10;
     }
 }
 
